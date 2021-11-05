3dc20c4a053d5aeb18d6d56fd89eb4b037de36c7
Luis Schulte <schulte4321l@outlook.de>
2021-11-05 10:13:06 +0100

    💄 🚧 Implemented User's Project List

--- src/pages/tables.tsx
+++ src/pages/tables.tsx
@@ -1,81 +1,131 @@
 import { useEffect, useState } from "react"
 import type { NextPage } from "next"
-
-import PeopleIcon from '@mui/icons-material/People'
-import GroupsIcon from '@mui/icons-material/Groups'
-import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount'
-import {
-    Box,
-    ToggleButtonGroup,
-    ToggleButton,
-    useTheme,
-} from "@mui/material"
-import {
-    DataGrid
-} from "@mui/x-data-grid"
-
+import AddIcon from "@mui/icons-material/Add"
+import { Box, ToggleButtonGroup, ToggleButton, useTheme } from "@mui/material"
 import Title from "@components/Head/Title"
-import {
-    CustomColumnMenuComponent,
-    CustomFooterComponent,
-    CustomToolbar,
-    CustomNoRowsOverlay,
-} from "@components/DataGrid/Custom"
-import { useTable, isTableType, Tables, Table } from "@lib/useTable"
+import { Data, useProject } from "@app/src/lib/useProject"
 
+const testData: Data = [
+    { project: "Projekt1", tables: ["Table1.1", "Table1.2", "Table1.3"] },
+    { project: "Projekt2", tables: ["Table2.1", "Table2.2", "Table2.3"] },
+    { project: "Projekt3", tables: ["Table3.1", "Table3.2", "Table3.3"] },
+]
 
-const TableIconMap: { [key in Table]: React.ReactNode } = {
-    Personen: <PeopleIcon />,
-    Organe: <GroupsIcon />,
-    Rollen: <SupervisorAccountIcon />
+// string used for `value` arg in button group to indicate a button which adds e.g. a new project or table
+
+type TablistProps = {
+    value: string | null
+    data: Array<string>
+    onChangeHandler: (val: string | null) => void
+    onAddHandler: (name: string) => void
+}
+const ADD_BUTTON_NAME = "__ADD__"
+
+const Tablist: React.FC<TablistProps> = props => {
+    const ADD_BUTTON_NAME = "__ADD__"
+    const theme = useTheme()
+
+    const handler = (_: unknown, val: string | null) => {
+        // BUG: when creating a new proj without tables, the add button's value will be null for unknown reason.
+        // because of that 'null' is catched and interpreteted as 'ADD_BUTTON_NAME'
+        // TODO: fix this
+        if ((typeof val === "string" && val === ADD_BUTTON_NAME) || val === null) {
+            const name = prompt("Choose new Name")
+            if (name) props.onAddHandler(name)
+        } else props.onChangeHandler(val)
+    }
+
+    return (
+        <ToggleButtonGroup
+            value={props.value || ADD_BUTTON_NAME}
+            exclusive
+            onChange={handler}
+            color="primary"
+            sx={{ display: "block", mb: theme.spacing(5) }}
+        >
+            {props.data.map((element, index) => (
+                <ToggleButton key={index} value={element}>
+                    {element}
+                </ToggleButton>
+            ))}
+            <ToggleButton key={props.data.length} value={ADD_BUTTON_NAME}>
+                <AddIcon />
+            </ToggleButton>
+        </ToggleButtonGroup>
+    )
 }
 
 const Test: NextPage = () => {
     const theme = useTheme()
+
     const status = "connected"
-    const { data, setTable } = useTable("Personen")
-    const handleTableChange =
-        (event: React.MouseEvent<HTMLElement>, newTableType: string | null) => {
-            if (newTableType && isTableType(newTableType))
-                setTable(newTableType)
-        }
+    const data = useProject(testData)
 
-    return ( <>
-        <Title title="Test" />
-        <Box>
-            <ToggleButtonGroup
-                value={data.tableType}
-                exclusive
-                onChange={handleTableChange}
-                color="primary">
-                {Tables.map( (tbl, index) =>
-                    <ToggleButton key={index} value={tbl}>
-                        {TableIconMap[tbl]}&nbsp;{tbl}
-                    </ToggleButton> )}
-            </ToggleButtonGroup>
-            <Box sx={{
-                mt: theme.spacing(5) }}>
-                <DataGrid
-                    rows={data.rows}
-                    columns={data.cols}
-                    pageSize={25}
-                    autoHeight
-                    rowsPerPageOptions={[5, 25, 50, 100]}
-                    checkboxSelection
-                    disableSelectionOnClick
-                    components={{
-                        Toolbar: CustomToolbar,
-                        NoRowsOverlay: CustomNoRowsOverlay,
-                        Footer: CustomFooterComponent,
-                        ColumnMenu: CustomColumnMenuComponent
-                    }}
-                    componentsProps={{
-                        footer: { status }
-                    }} />
+    const handleProjectChange = (newProject: string | null) => {
+        if (newProject) data.changeProject(newProject)
+    }
+    const handleAddProject = (newProject: string) => {
+        testData.push({ project: newProject, tables: [] })
+        data.refresh(testData)
+    }
+
+    const handleTableChange = (newTable: string | null) => {
+        if (newTable) data.changeTable(newTable)
+    }
+    const handleAddTable = (newTable: string) => {
+        testData.forEach(proj => {
+            if (proj.project === data.project) {
+                proj.tables.push(newTable)
+            }
+        })
+        data.refresh(testData)
+    }
+
+    return (
+        <>
+            <Title title="Test" />
+            <Box>
+                {/* Projects */}
+                <Tablist
+                    value={data.project}
+                    data={data.projects}
+                    onChangeHandler={handleProjectChange}
+                    onAddHandler={handleAddProject}
+                />
+
+                {/* Tables */}
+                <Tablist
+                    value={data.table}
+                    data={data.tables}
+                    onChangeHandler={handleTableChange}
+                    onAddHandler={handleAddTable}
+                />
+
+                {/* Table */}
+                <Box>
+                    {data.project} - {data.table}
+                    {/* <DataGrid
+                        rows={data.rows}
+                        columns={data.cols}
+                        pageSize={25}
+                        autoHeight
+                        rowsPerPageOptions={[5, 25, 50, 100]}
+                        checkboxSelection
+                        disableSelectionOnClick
+                        components={{
+                            Toolbar: CustomToolbar,
+                            NoRowsOverlay: CustomNoRowsOverlay,
+                            Footer: CustomFooterComponent,
+                            ColumnMenu: CustomColumnMenuComponent,
+                        }}
+                        componentsProps={{
+                            footer: { status },
+                        }}
+                    /> */}
+                </Box>
             </Box>
-        </Box>
-    </> )
+        </>
+    )
 }
-
 
 export default Test
