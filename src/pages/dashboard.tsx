4ffb8e647ede0d9fb228f988b7628d120864a3c4
Luis Schulte <schulte4321l@outlook.de>
2021-11-05 11:01:49 +0100

    refactor(pages): renamed next page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
-import type { NextPage } from "next"
+import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next"
 import AddIcon from "@mui/icons-material/Add"
 import { Box, ToggleButtonGroup, ToggleButton, useTheme } from "@mui/material"
 import Title from "@components/Head/Title"
 import { Data, useProject } from "@app/src/utils/useProject"
+import { getProjects } from "../utils/getData"
 
 const testData: Data = [
     { project: "Projekt1", tables: ["Table1.1", "Table1.2", "Table1.3"] },
@@ -11,15 +12,12 @@ const testData: Data = [
     { project: "Projekt3", tables: ["Table3.1", "Table3.2", "Table3.3"] },
 ]
 
-// string used for `value` arg in button group to indicate a button which adds e.g. a new project or table
-
 type TablistProps = {
     value: string | null
     data: Array<string>
     onChangeHandler: (val: string | null) => void
     onAddHandler: (name: string) => void
 }
-const ADD_BUTTON_NAME = "__ADD__"
 
 const Tablist: React.FC<TablistProps> = props => {
     const ADD_BUTTON_NAME = "__ADD__"
@@ -29,7 +27,7 @@ const Tablist: React.FC<TablistProps> = props => {
         // BUG: when creating a new proj without tables, the add button's value will be null for unknown reason.
         // because of that 'null' is catched and interpreteted as 'ADD_BUTTON_NAME'
         // TODO: fix this
-        // Note: this causes a bug, where clicking on a already selected toggle button will pass the above and prompt
+        // NOTE: this causes a bug, where clicking on a already selected toggle button will pass the above if and prompt for adding a new entitiy
         if ((typeof val === "string" && val === ADD_BUTTON_NAME) || val === null) {
             const name = prompt("Choose new Name")
             if (name) props.onAddHandler(name)
@@ -56,11 +54,9 @@ const Tablist: React.FC<TablistProps> = props => {
     )
 }
 
-const Test: NextPage = () => {
-    const theme = useTheme()
-
-    const status = "connected"
+const Dashboard: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = props => {
     const data = useProject(testData)
+    // const data = useProject(props.data) // wont work w/ updates
 
     const handleProjectChange = (newProject: string | null) => {
         if (newProject) data.changeProject(newProject)
@@ -68,6 +64,7 @@ const Test: NextPage = () => {
     const handleAddProject = (newProject: string) => {
         testData.push({ project: newProject, tables: [] })
         data.refresh(testData)
+        console.log(data.projects)
     }
 
     const handleTableChange = (newTable: string | null) => {
@@ -81,6 +78,8 @@ const Test: NextPage = () => {
         })
         data.refresh(testData)
     }
+
+    useEffect(() => {}, [])
 
     return (
         <>
@@ -129,4 +128,26 @@ const Test: NextPage = () => {
     )
 }
 
-export default Test
+type DashboardPage_ServerSideProps = {
+    data: Data
+}
+
+export const getServerSideProps: GetServerSideProps<DashboardPage_ServerSideProps> =
+    async context => {
+        const { params, locale, defaultLocale } = context
+
+        const data = await getProjects()
+        console.log(data)
+
+        if (false) {
+            return { notFound: true }
+        }
+
+        return {
+            props: {
+                data: testData,
+            },
+        }
+    }
+
+export default Dashboard
