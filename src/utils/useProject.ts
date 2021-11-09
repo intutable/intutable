afe6fa87e3b2dd08ed0ca319b072e4c46e8eef1c
Luis Schulte <schulte4321l@outlook.de>
2021-11-09 16:39:46 +0100

    refactor(useTable): delete hook

--- src/utils/useProject.ts
+++ src/utils/useProject.ts
@@ -2,45 +2,6 @@ import { useState, useEffect } from "react"
 import type { GridColDef, GridValueGetterParams, GridRowData, GridColumns } from "@mui/x-data-grid"
 import { getDataForTable } from "./getData"
 
-export const Tables = ["Personen", "Organe", "Rollen"] as const
-
-export type Table = typeof Tables[number]
-export const isTableType = (str: string): str is Table => Tables.includes(str as Table)
-
-export type DataGridDataType = {
-    tableType: Table
-    rows: Array<GridRowData>
-    cols: GridColumns
-}
-const DataGridDataPlaceholder = {
-    row: [],
-    col: [{ field: "id", headerName: "ID" }],
-}
-
-/**
- * Fetches the data for the specified table and returns it.
- * @param {Table} tableType type of the table .
- * @returns {{DataGridDataType,Dispatch<SetStateAction<Table>>}} Object with the data and a function to change the table type.
- */
-export const useTable = (tableType: Table) => {
-    const [table, setTable] = useState<Table>(tableType)
-    const [data, setData] = useState<DataGridDataType>({
-        tableType: tableType,
-        rows: DataGridDataPlaceholder.row,
-        cols: DataGridDataPlaceholder.col,
-    })
-    useEffect(() => {
-        // fetch data related to table type
-        async function getData() {
-            const data = await getDataForTable()
-            setData(data as any)
-        }
-        getData()
-    }, [table])
-
-    return { data, setTable }
-}
-
 // only for dev
 export type Data = Array<{ project: string; tables: Array<string> }>
 const INITIAL_CHOSEN_PROJECT = 0
