dd1217d54f802e6e5fae2d0d57e78b2ee09dba03
Luis Schulte <schulte4321l@outlook.de>
2021-11-11 15:10:36 +0100

    feat: implement new project list view (closes this issue)

---
+++ src/pages/project/[...project-slug].tsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from "react"
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next"
+import Title from "../../components/Head/Title"
+import { CircularProgress, Typography, useTheme, Box } from "@mui/material"
+import { Tablist, ADD_BUTTON_TOKEN } from "../../components/TabList/TabList"
+import DataGrid from "react-data-grid"
+import { getDataOfTable, getTablesOfProject, TableData } from "../../utils/getData"
+import { useSnackbar } from "notistack"
+import { isValidName, prepareName } from "../../utils/validateName"
+
+type ProjectSlugPageProps = {
+    project: string
+    tables: Array<string>
+}
+const ProjectSlugPage: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = props => {
+    const [_tables, _setTables] = useState<Pick<ProjectSlugPageProps, "tables">["tables"]>(
+        props.tables
+    )
+    const theme = useTheme()
+    const { enqueueSnackbar } = useSnackbar()
+
+    const [tableData, setTableData] = useState<TableData | null>(null)
+    const [currentTable, setCurrentTable] = useState<string>(_tables[0] || ADD_BUTTON_TOKEN)
+    const [loading, setLoading] = useState<boolean>(true)
+
+    const handleTableChange = (newTable: string | null) => {
+        if (newTable) setCurrentTable(newTable)
+    }
+
+    const handleAddTable = (newTableName: string) => {
+        const name = prepareName(newTableName)
+        const isValid = isValidName(name)
+        if (isValid instanceof Error) return enqueueSnackbar(isValid.message, { variant: "error" })
+        const nameIsTaken = _tables.map(tbl => tbl.toLowerCase()).includes(name.toLowerCase())
+        if (nameIsTaken)
+            return enqueueSnackbar(
+                "Dieser Name wird bereits für eine Tabelle in diesem Projekt verwendet!",
+                { variant: "error" }
+            )
+        // NOTE: this request is blocking (the useEffect on `currentTable` wont be called until this fetch finished).
+        // NOTE: this request must also create an empty table in the backend which gets fetched right after, otherwise this will lead to an error
+        // TODO: make a request to backend here and then select new table
+        _setTables(prev => [...prev, name])
+        setTableData(null)
+        setCurrentTable(name)
+        enqueueSnackbar(`Du hast erfolgreich '${name}' erstellt!`, { variant: "success" })
+    }
+
+    useEffect(() => {
+        ;(async _ => {
+            try {
+                setLoading(true)
+                const serverRequest = await getDataOfTable(currentTable)
+                setTableData(serverRequest)
+            } catch (error) {
+                enqueueSnackbar("Fehler: Die Tabelle konnte nicht geladen werden!", {
+                    variant: "error",
+                })
+                setTableData(null)
+            } finally {
+                setLoading(false)
+            }
+        })()
+    }, [currentTable])
+
+    return (
+        <>
+            <Title title={props.project} />
+            <Typography variant="h5" sx={{ mb: theme.spacing(4) }}>
+                {props.project}
+            </Typography>
+            <Tablist
+                value={currentTable}
+                data={_tables}
+                onChangeHandler={handleTableChange}
+                onAddHandler={handleAddTable}
+            />
+            {loading ? (
+                <CircularProgress />
+            ) : tableData ? (
+                // TODO: seperate this into a new component which takes care of sending updates to the backend
+                <Box>
+                    <DataGrid rows={tableData.rows} columns={tableData.cols} />
+                </Box>
+            ) : (
+                <>Could not load the Table!</>
+            )}
+        </>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps<ProjectSlugPageProps> = async context => {
+    const { params } = context
+
+    const user = { name: "nick@baz.org" } // TODO: get user
+
+    const serverRequet = await getTablesOfProject(user, params["project-slug"][0])
+
+    const data: ProjectSlugPageProps = {
+        project: params["project-slug"][0],
+        tables: serverRequet,
+    }
+
+    const error = serverRequet == null
+    if (error) return { notFound: true }
+
+    return {
+        props: data,
+    }
+}
+
+export default ProjectSlugPage
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,35 +1,107 @@
-import { NextPage } from "next"
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next"
 import Title from "../components/Head/Title"
-import { CircularProgress, Grid, Card, CardContent } from "@mui/material"
+import { CircularProgress, Grid, Card, CardContent, Typography } from "@mui/material"
 import React from "react"
-import { useProject } from "../utils/useProject"
+import { useRouter } from "next/dist/client/router"
+import { useTheme } from "@mui/material"
+import AddIcon from "@mui/icons-material/Add"
+import { isValidName, prepareName } from "../utils/validateName"
+import { useSnackbar } from "notistack"
+import { getProjects } from "../utils/getData"
 
-type ProjectCardProps = {}
+type ProjectCardProps = {
+    url?: string
+    onClick?: () => void
+}
 const ProjectCard: React.FC<ProjectCardProps> = props => {
+    const router = useRouter()
+    const theme = useTheme()
+
     return (
-        <Card>
+        <Card
+            onClick={props.onClick || (_ => props.url && router.push("/project/" + props.url))}
+            sx={{
+                minWidth: 150,
+                minHeight: 150,
+                cursor: "pointer",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                "&:hover": {
+                    bgcolor: theme.palette.action.hover,
+                },
+            }}
+        >
             <CardContent>{props.children}</CardContent>
         </Card>
     )
 }
 
-const ProjectsPage: NextPage = () => {
-    const { projects } = useProject()
+type ProjectsPageProps = {
+    projects: Array<string>
+}
+const ProjectsPage: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = props => {
+    const theme = useTheme()
+    const router = useRouter()
+    const { enqueueSnackbar } = useSnackbar()
 
-    if (projects.length === 0) return <CircularProgress />
+    const handleAddProject = () => {
+        const namePrompt = prompt("Benenne Dein neues Projekt!")
+        const name = prepareName(namePrompt)
+        const isValid = isValidName(name)
+        if (isValid instanceof Error) return enqueueSnackbar(isValid.message, { variant: "error" })
+        const nameIsTaken = props.projects
+            .map(proj => proj.toLowerCase())
+            .includes(name.toLowerCase())
+        if (nameIsTaken)
+            return enqueueSnackbar(
+                "Dieser Name wird bereits für eines deiner Projekte verwendet!",
+                { variant: "error" }
+            )
+        // TODO: make a request to backend here and then redirect to project
+        router.push("/project/" + name)
+        enqueueSnackbar(`Du hast erfolgreich '${name}' erstellt!`, { variant: "success" })
+    }
 
     return (
         <>
             <Title title="Projekte" />
+            <Typography variant="h5" sx={{ mb: theme.spacing(4) }}>
+                Deine Projekte
+            </Typography>
             <Grid container spacing={2}>
-                {projects.map(proj => (
-                    <Grid item xs={4}>
-                        <ProjectCard>{proj}</ProjectCard>
+                {props.projects.map((proj, i) => (
+                    <Grid item key={i}>
+                        <ProjectCard url={proj}>{proj}</ProjectCard>
                     </Grid>
                 ))}
+                <Grid item>
+                    <ProjectCard onClick={handleAddProject}>
+                        <AddIcon />
+                    </ProjectCard>
+                </Grid>
             </Grid>
         </>
     )
+}
+
+export const getServerSideProps: GetServerSideProps<ProjectsPageProps> = async context => {
+    const { params } = context
+
+    const user = { name: "nick@baz.org" } // TODO: get user
+
+    const serverRequest = await getProjects(user)
+
+    const data: ProjectsPageProps = {
+        projects: serverRequest,
+    }
+
+    const error = serverRequest == null
+    if (error) return { notFound: true }
+
+    return {
+        props: data,
+    }
 }
 
 export default ProjectsPage
