cf03ce42a3577d6a8d7638e4eb12dbb1855ea8ed
Luis Schulte <schulte4321l@outlook.de>
2021-11-24 13:18:06 +0100

    feat: add loading skeleton

---
+++ src/components/DataGrid/LoadingSkeleton/LoadingSkeleton.tsx
@@ -0,0 +1,9 @@
+import Skeleton from "@mui/material/Skeleton"
+import Stack from "@mui/material/Stack"
+export default () => (
+    <Stack spacing={1}>
+        <Skeleton variant="text" />
+        <Skeleton variant="circular" width={40} height={40} />
+        <Skeleton variant="rectangular" width={210} height={118} />
+    </Stack>
+)
