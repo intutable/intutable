701ac30c020cc57314bf190c521937f762a73402
Luis Schulte <schulte4321l@outlook.de>
2021-11-24 19:53:21 +0100

    fix: lint error

--- src/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Typography, Button, Switch, FormControlLabel, useTheme, PaletteMode } from "@mui/material"
 
 type ThemeSwitchProps = {
