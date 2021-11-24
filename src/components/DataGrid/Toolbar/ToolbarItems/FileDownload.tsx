61d6c77776a88765d5d47c4c30ef645952409459
Luis Schulte <schulte4321l@outlook.de>
2021-11-24 13:25:12 +0100

    feat: add context menu for file download in toolbar

--- src/components/DataGrid/Toolbar/ToolbarItems/FileDownload.tsx
+++ src/components/DataGrid/Toolbar/ToolbarItems/FileDownload.tsx
@@ -1,6 +1,40 @@
 import type { PredefinedToolbarItem } from "../types"
-import { IconButton, useTheme } from "@mui/material"
+import { IconButton, useTheme, Menu, MenuItem } from "@mui/material"
 import FileDownloadIcon from "@mui/icons-material/FileDownload"
+import React, { useState } from "react"
+
+type FileDownoadContextMenuProps = {
+    anchorEL: Element
+    open: boolean
+    onClose: () => void
+    children: Array<React.ReactNode> | React.ReactNode // overwrite implicit `children`
+}
+const FileDownloadContextMenu: React.FC<FileDownoadContextMenuProps> = props => {
+    const theme = useTheme()
+
+    return (
+        <Menu
+            elevation={0}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            // transformOrigin={{ vertical: "top", horizontal: "right" }}
+            open={props.open}
+            anchorEl={props.anchorEL}
+            keepMounted={true}
+            onClose={props.onClose}
+            PaperProps={{
+                sx: {
+                    boxShadow: theme.shadows[1],
+                },
+            }}
+        >
+            {Array.isArray(props.children) ? (
+                props.children.map((item, i) => <MenuItem key={i}>{item}</MenuItem>)
+            ) : (
+                <MenuItem>{props.children}</MenuItem>
+            )}
+        </Menu>
+    )
+}
 
 type FileDownloadProps = {}
 
@@ -10,12 +44,27 @@ type FileDownloadProps = {}
 const FileDownload: PredefinedToolbarItem<FileDownloadProps> = props => {
     const theme = useTheme()
 
-    const handleOnClick = () => {}
+    const [anchorEL, setAnchorEL] = useState<HTMLElement | null>(null)
+
+    const handleOnClick = event => {
+        event.preventDefault()
+        setAnchorEL(event.currentTarget)
+    }
+    const handleCloseContextMenu = () => setAnchorEL(null)
 
     return (
-        <IconButton onClick={handleOnClick}>
-            <FileDownloadIcon />
-        </IconButton>
+        <>
+            <IconButton onClick={handleOnClick}>
+                <FileDownloadIcon />
+            </IconButton>
+            <FileDownloadContextMenu
+                anchorEL={anchorEL}
+                open={anchorEL != null}
+                onClose={handleCloseContextMenu}
+            >
+                Test
+            </FileDownloadContextMenu>
+        </>
     )
 }
 export default FileDownload
