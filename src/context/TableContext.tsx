aef7486387488d9e543c42711f2eeb7e2d686eec
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2021-12-09 17:02:38 +0100

    chore: work on table context

--- src/context/TableContext.tsx
+++ src/context/TableContext.tsx
@@ -9,13 +9,48 @@ const TableContext = React.createContext<TableContextProps>(initialState)
 
 export const useTable = () => React.useContext(TableContext)
 
-export const TableProvider: React.FC = props => {
+type TableProviderProps = {
+    tableList: string[]
+}
+
+export const TableProvider: React.FC<TableProviderProps> = props => {
     const router = useRouter()
 
-    useEffect(() => {}, [])
+    const [currentTable, setCurrentTable] = useState<string | null>(null)
+    const [tableData, setTableData] = useState<unknown>(null)
+
+    useEffect(() => {
+        if (!currentTable && tableData != null) setTableData(null)
+
+        if (currentTable && tableData == null) {
+            ;(async () => {
+                // TODO: fetch table data
+            })()
+        }
+    }, [currentTable])
+
+    const reset = () => {
+        setCurrentTable(null)
+        setTableData(null)
+    }
+
+    const changeTable = (table: string) => {
+        if (props.tableList.includes(table)) {
+            setCurrentTable(table)
+        }
+    }
+
+    const reloadProject = () => {}
+    const reloadTable = () => {}
 
     return (
-        <TableContext.Provider value={{}}>
+        <TableContext.Provider
+            value={{
+                table: currentTable,
+                tableData,
+                reset,
+            }}
+        >
             {props.children}
         </TableContext.Provider>
     )
