1c21253d4da6b815f92fc6dd2defbe372cf882f8
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-01-05 13:55:16 +0100

    feat: implement TableContext

--- src/context/TableContext.tsx
+++ src/context/TableContext.tsx
@@ -1,57 +1,103 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useCallback, useContext, useEffect, useState } from "react"
 import { useRouter } from "next/router"
+import { getTableData, TableData } from "@app/api"
+import { useAuth } from "./AuthContext"
 
 export type TableContextProps = {}
-
 const initialState: TableContextProps = {}
-
 const TableContext = React.createContext<TableContextProps>(initialState)
 
-export const useTable = () => React.useContext(TableContext)
-
 type TableProviderProps = {
-    tableList: string[]
+    projectName: string
+    projectTables: string[]
 }
 
+/**
+ *
+ * @param props
+ * @returns
+ */
 export const TableProvider: React.FC<TableProviderProps> = props => {
     const router = useRouter()
+    const { getUserAuthCookie } = useAuth()
 
-    const [currentTable, setCurrentTable] = useState<string | null>(null)
-    const [tableData, setTableData] = useState<unknown>(null)
+    // #################### states ####################
+
+    const [projectTables, setProjectTables] = useState<string[]>(
+        props.projectTables
+    ) // all available tables in a project
+    const [currentTable, setCurrentTable] = useState<string | null>(null) // the current table, if 'null' it is loading or the project has no tables
+    const [tableData, setTableData] = useState<TableData | null>(null) // data of the current table
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<null | Error>(null)
+
+    // #################### private methods ####################
+
+    const fetchData = useCallback(async () => {
+        setLoading(true)
+        if (getUserAuthCookie && getUserAuthCookie() && currentTable) {
+            const authCookie = getUserAuthCookie() || undefined
+            const fetchedData = await getTableData(
+                currentTable,
+                props.projectName,
+                authCookie
+            )
+            setTableData(fetchedData)
+        } else {
+            setError(
+                new Error(
+                    "Could not get the user cookie in 'TableContext.tsx:fetchData'!"
+                )
+            )
+        }
+        setLoading(false)
+    }, [currentTable, getUserAuthCookie])
+
+    // #################### life cycle ####################
 
     useEffect(() => {
         if (!currentTable && tableData != null) setTableData(null)
-
-        if (currentTable && tableData == null) {
-            ;(async () => {
-                // TODO: fetch table data
-            })()
+        if (currentTable) {
+            fetchData()
         }
     }, [currentTable])
 
-    const reset = () => {
-        setCurrentTable(null)
-        setTableData(null)
+    // #################### public methods ####################
+
+    /**
+     * Changes the current tables and loads the data.
+     * @param {string} table must be in the list of available project tables.
+     */
+    const changeTable = (table: string): void => {
+        if (!projectTables.includes(table))
+            throw new RangeError(
+                `Table '${table} is not a member of the current project!`
+            )
+        setCurrentTable(table)
     }
 
-    const changeTable = (table: string) => {
-        if (props.tableList.includes(table)) {
-            setCurrentTable(table)
-        }
-    }
+    /**
+     * Reloads the data.
+     */
+    const refresh = () => {}
 
-    const reloadProject = () => {}
-    const reloadTable = () => {}
+    // #################### provider ####################
 
     return (
         <TableContext.Provider
             value={{
-                table: currentTable,
+                availableTables: projectTables,
+                currentTable,
                 tableData,
-                reset,
+                loading,
+                error,
+                changeTable,
+                refresh,
             }}
         >
             {props.children}
         </TableContext.Provider>
     )
 }
+
+export const useTable = () => React.useContext(TableContext)
