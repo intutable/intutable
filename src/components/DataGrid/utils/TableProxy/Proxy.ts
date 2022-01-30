5439bdfd260eb8ad79b9fdecd750b24d885c2a42
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-01-30 10:36:29 +0100

    refactor(TableProxy): merge files

--- src/components/DataGrid/utils/TableProxy/Proxy.ts
+++ src/components/DataGrid/utils/TableProxy/Proxy.ts
@@ -1,6 +1,11 @@
-import { TableData } from "@app/api"
+import { TableData as Table } from "@app/api"
 
 export {}
+
+export const makeObservableVirtualTable = (
+    target: Table,
+    observer: (change: string) => {}
+) => {}
 
 /**
  * Create a Proxy that covers operations on table data and use this proxy widley in the application.
@@ -9,16 +14,49 @@ export {}
  * • implement private properties that indicate the type etc.
  */
 
-// let table: TableData = {}
-const table = {}
+/**
+ * Überglegung: Eine `VirtualTable` nutzen, um Daten zu updaten.
+ * Diese wird als State in React benutzt und reagiert auf Änderungen, indem diese validiert (`ProxyValidator`) und geupdatet werden.
+ *
+ *
+ * • Private Props für Typ, Update-Status etc. Dann einen zusätzlichen State in React der auf diese Update-Prop referiert, die Updates pusht etc.
+ *
+ * • kann überprüfen, wenn die Daten einer Eigenschaft einer Row verändert werden, ob diese auch in der entsprechenden Col definiert sind.
+ *
+ * • lazy initialization: weitere Rows erst laden, wenn sie benötigt werden
+ *
+ * • das Interface könnte bspw. wie folgt aussehen:
+ *
+ * <pre>
+ * {
+ *  [rowID: number]: {
+ *      data: {…} // get: Props gegenchecken mit Col und Daten aus Row
+ *      meta: {…} // get: aus der entsprechenden Col gebildet
+ *  }
+ * }
+ * </pre>
+ *
+ * für SET wird ein Validator eingesetzt.
+ *
+ */
+
 // eslint-disable-next-line no-undef
-const test = new Proxy(table, {
-    get: (target, property, receiver) => {
-        return Reflect.get(target, property)
-    },
-    isExtensible: target => false,
-    preventExtensions: target => {
-        Object.preventExtensions(target)
-        return true
-    },
-})
+const VirtualTable = new Proxy(
+    {},
+    {
+        set: (target, property) => {
+            return Reflect.get(target, property)
+        },
+        has: (target, property) => {
+            return Reflect.has(target, property)
+        },
+        get: (target, property, receiver) => {
+            return Reflect.get(target, property)
+        },
+        isExtensible: target => false,
+        preventExtensions: target => {
+            Object.preventExtensions(target)
+            return true
+        },
+    }
+)
