bde529eb635a485aa00bf633bfe31b40784d64e6
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-03-23 15:05:13 +0100

    fix(lint): error Co-authored-by: Kilian Folger <kilulafo@gmail.com>

--- src/backend/start/core.ts
+++ src/backend/start/core.ts
@@ -1,17 +1,11 @@
+import { Core, EventSystem } from "@intutable/core"
+import net from "net"
 import path from "path"
 import process from "process"
-import net from "net"
-
-import { Core, EventSystem } from "@intutable/core"
-import cors from "cors"
-import { addMiddleware } from "@intutable/http"
-
-import { getFrontendUrl } from "../runtimeconfig"
 
 const PLUGIN_PATHS = [path.join(process.cwd(), "node_modules/@intutable/*")]
 const PG_PORT = 5432
-const RETRIES = 10
-
+const RETRIES = Math.pow(2, 30)
 
 main()
 
@@ -20,47 +14,50 @@ main()
  * running and listen for requests.
  */
 async function main() {
-    await waitForDatabase()
-        .catch(e => crash<Core>(e))
+    await waitForDatabase().catch(e => crash<Core>(e))
     const events: EventSystem = new EventSystem(true) // debug mode
-    const core: Core = await Core.create(PLUGIN_PATHS, events)
-        .catch(e => crash<Core>(e)
-    )
+    await Core.create(PLUGIN_PATHS, events).catch(e => crash<Core>(e))
 }
 
 async function waitForDatabase() {
-    let connected: boolean = false
+    let connected = false
     let lastError: unknown
     let retries = RETRIES
-    while(retries > 0 && !connected){
+    while (retries > 0 && !connected) {
         console.log(`${path.basename(__filename)}: waiting for database...`)
         console.log(`${path.basename(__filename)}: retries: ${retries}`)
         await testPort(PG_PORT)
-            .then(r => { connected = true })
-            .catch(e => { lastError = e })
-        await new Promise((res, rej) => setTimeout(res, 3000))
+            .then(() => {
+                connected = true
+            })
+            .catch(e => {
+                lastError = e
+            })
+        await new Promise(res => setTimeout(res, 3000))
         retries--
     }
-    if (connected){
+    if (connected) {
         return
     } else {
-        return Promise.reject({ error: {
-            message: "could not connect to database",
-            reason: lastError
-        }})
+        return Promise.reject({
+            error: {
+                message: "could not connect to database",
+                reason: lastError,
+            },
+        })
     }
 }
 
-async function testPort(port, host?){
+async function testPort(port, host?) {
     let socket: net.Socket
     return new Promise((res, rej) => {
         socket = net.createConnection(port, host)
         socket
-            .on("connect", function(e){
+            .on("connect", function (e) {
                 res(e)
                 socket.destroy()
             })
-            .on("error", function(e){
+            .on("error", function (e) {
                 rej(e)
                 socket.destroy()
             })
