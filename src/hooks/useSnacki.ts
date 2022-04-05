0c2df81087c7edfadd7c3919d15a9c91ef644be9
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-04-05 17:44:45 +0200

    feat(useSnacki): new custom hook with predefined snacksbar variants

---
+++ src/hooks/useSnacki.ts
@@ -0,0 +1,30 @@
+import { OptionsObject, useSnackbar } from "notistack"
+
+export const useSnacki = () => {
+    const { enqueueSnackbar } = useSnackbar()
+
+    const snackError = (
+        message: string,
+        options?: Omit<OptionsObject, "variant">
+    ) => enqueueSnackbar(message, { variant: "error", ...options })
+
+    const snackWarning = (
+        message: string,
+        options?: Omit<OptionsObject, "variant">
+    ) => enqueueSnackbar(message, { variant: "warning", ...options })
+
+    const snackSuccess = (
+        message: string,
+        options?: Omit<OptionsObject, "variant">
+    ) => enqueueSnackbar(message, { variant: "success", ...options })
+
+    const snackInfo = (
+        message: string,
+        options?: Omit<OptionsObject, "variant">
+    ) => enqueueSnackbar(message, { variant: "info", ...options })
+
+    const snack = (message: string, options?: Omit<OptionsObject, "variant">) =>
+        enqueueSnackbar(message, { variant: "default", ...options })
+
+    return { snackError, snackWarning, snackSuccess, snackInfo, snack }
+}
