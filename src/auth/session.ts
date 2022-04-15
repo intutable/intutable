33425df793987958050973d407aacc625fd958a3
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-04-15 12:24:52 +0200

    chore: adjust iron session options

--- src/auth/session.ts
+++ src/auth/session.ts
@@ -6,8 +6,10 @@ import type { User } from "types/User"
 export const sessionOptions: IronSessionOptions = {
     password: "6RAw0qN^H=?9G~TtWC2A,,n3-T%,zM*-Q]iZ~L8#", // TODO: store this in encrypted env file
     cookieName: process.env.NEXT_PUBLIC_AUTH_COOKIE_KEY as string,
-    ttl: 3600,
+    // ttl: 3600,
     cookieOptions: {
+        httpOnly: true,
+        // sameSite: "lax",
         // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
         // secure: process.env.NODE_ENV === "production",
         secure: false,
