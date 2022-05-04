7905e0ed8b4cd5a417395385e12c1a3fd5d26801
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-05-04 15:44:33 +0200

    refactor: renamed useTableData hook -> useTable

--- src/hooks/useTable.ts
+++ src/hooks/useTable.ts
@@ -2,9 +2,7 @@ import { ColumnInfo, TableDescriptor } from "@intutable/lazy-views"
 import useSWR from "swr"
 import { Column, TableData } from "types"
 
-export const useTableData = (
-    tableId: TableDescriptor["id"] | null | undefined
-) => {
+export const useTable = (tableId: TableDescriptor["id"] | null | undefined) => {
     const { data, error, mutate } = useSWR<TableData>(
         tableId
             ? {
