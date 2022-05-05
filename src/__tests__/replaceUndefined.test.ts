522d771ef2235e9ca502f0c660d0c4b123fb80c9
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-05-05 20:31:29 +0200

    fix: test

--- src/__tests__/replaceUndefined.test.ts
+++ src/__tests__/replaceUndefined.test.ts
@@ -2,18 +2,19 @@ import { replaceUndefined } from "../utils/replaceUndefined"
 
 describe("'replaceUndefined' utility", () => {
     it("should recursivley replace 'undefined' by 'null'", () => {
+        const date = new Date()
         const sample = {
             key1: "hello",
             key2: 1,
             key3: true,
-            key4: new Date(),
+            key4: date,
             key5: null,
             key6: undefined,
             key7: {
                 key7_1: "hello",
                 key7_2: 1,
                 key7_3: true,
-                key7_4: new Date(),
+                key7_4: date,
                 key7_5: null,
                 key7_6: undefined,
             },
@@ -22,14 +23,14 @@ describe("'replaceUndefined' utility", () => {
             key1: "hello",
             key2: 1,
             key3: true,
-            key4: new Date(),
+            key4: date,
             key5: null,
             key6: null,
             key7: {
                 key7_1: "hello",
                 key7_2: 1,
                 key7_3: true,
-                key7_4: new Date(),
+                key7_4: date,
                 key7_5: null,
                 key7_6: null,
             },
