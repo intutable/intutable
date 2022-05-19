7a576313512761b3a2e374b064d0a6d79dd15ed0
Kilian Folger <kilulafo@gmail.com>
2022-05-19 02:20:44 +0200

    refactor: put test-data-setup in separate folder

---
+++ src/backend/example/load.ts
@@ -0,0 +1,159 @@
+import { PluginLoader } from "@intutable/core"
+import { insert } from "@intutable/database/dist/requests"
+import {
+    ProjectDescriptor,
+    TableDescriptor,
+    TableInfo,
+    ColumnDescriptor,
+} from "@intutable/project-management/dist/types"
+import {
+    createProject,
+    createTableInProject,
+    getTableInfo,
+    createColumnInTable,
+} from "@intutable/project-management/dist/requests"
+import { requests as v_req } from "@intutable/lazy-views/"
+import { types as v_types } from "@intutable/lazy-views"
+import { tableId, viewId } from "@intutable/lazy-views"
+
+import {
+    TableSpec,
+    JoinSpec,
+    Table,
+    PK_COLUMN,
+    PERSONEN,
+    PERSONEN_DATA,
+    ORGANE,
+    ORGANE_DATA,
+    ROLLEN,
+    ROLLEN_DATA,
+} from "./schema"
+
+
+let personen: Table
+let organe: Table
+let simpleTables: Table[]
+let rollen: Table
+
+export async function createExampleSchema(
+    core: PluginLoader,
+    adminId: number
+): Promise<void> {
+    const project: ProjectDescriptor = await core.events.request(
+        createProject(adminId, "Fakultät MathInf")
+    ) as ProjectDescriptor
+    personen = await createTable(core, adminId, project.id, PERSONEN)
+    organe = await createTable(core, adminId, project.id, ORGANE)
+    simpleTables = [ personen, organe ]
+    rollen = await createTable(core, adminId, project.id, ROLLEN)
+}
+async function createTable(
+    core: PluginLoader,
+    userId: number,
+    projectId: number,
+    table: TableSpec
+): Promise<Table> {
+    const baseTable: TableDescriptor = await core.events.request(
+        createTableInProject(
+            userId,
+            projectId,
+            table.name,
+            table.columns.map(c => c.baseColumn)
+        )
+    ) as TableDescriptor
+    const tableInfo = await core.events.request(
+        getTableInfo(baseTable.id)
+    ) as TableInfo
+    const idColumn = tableInfo.columns.find(c => c.name === PK_COLUMN)
+    const viewColumns: v_types.ColumnSpecifier[] = table.columns.map(c => {
+        const baseColumn = tableInfo.columns.find(parent =>
+            parent.name === c.baseColumn.name)!
+        return {
+            parentColumnId: baseColumn.id,
+            attributes: c.attributes
+        }
+    })
+    const tableView = await core.events.request(
+        v_req.createView(
+            tableId(baseTable.id),
+            table.name,
+            { columns: viewColumns, joins: [] },
+            EMPTY_ROW_OPTIONS
+        )
+    ) as v_types.ViewDescriptor
+    const filterView = await core.events.request(
+        v_req.createView(
+            viewId(tableView.id),
+            "Standard",
+            { columns: [], joins: [] },
+            baseRowOptions(idColumn)
+        )
+    )
+    const tableDescriptors = { baseTable, tableView, filterView }
+    await Promise.all(
+        table.joins.map(j => addJoin(core, tableDescriptors, j))
+    )
+    return tableDescriptors
+}
+
+async function addJoin(
+    core: PluginLoader,
+    table: Table,
+    join: JoinSpec
+): Promise<void> {
+    const fk = await core.events.request(
+        createColumnInTable(
+            table.baseTable.id,
+            join.fkColumn.name,
+            join.fkColumn.type
+        )
+    ) as ColumnDescriptor
+    const foreignTable = simpleTables.find(
+        t => t.tableView.name === join.table)!
+    const info = await core.events.request(
+        v_req.getViewInfo(foreignTable.tableView.id)
+    ) as TableInfo
+    const pk = info.columns.find(c => c.name === join.pkColumn)!
+    const foreignColumns = join.linkColumns.map(l => {
+        const parentColumn = info.columns.find(c => c.name === l.name)!
+        return {
+            parentColumnId: parentColumn.id,
+            attributes: l.attributes
+        }
+    })
+    await core.events.request(
+        v_req.addJoinToView(table.tableView.id, {
+            foreignSource: viewId(foreignTable.tableView.id),
+            on: [fk.id, "=", pk.id],
+            columns: foreignColumns
+        })
+    )
+}
+
+export async function insertExampleData(core: PluginLoader): Promise<void> {
+    await Promise.all(PERSONEN_DATA.map(r => core.events.request(
+        insert(personen.baseTable.key, r)
+    )))
+    await Promise.all(ORGANE_DATA.map(r => core.events.request(
+        insert(organe.baseTable.key, r)
+    )))
+    await Promise.all(ROLLEN_DATA.map(r => core.events.request(
+        insert(rollen.baseTable.key, r)
+    )))
+}
+
+const EMPTY_ROW_OPTIONS: v_types.RowOptions = {
+    conditions: [],
+    groupColumns: [],
+    sortColumns: []
+}
+function baseRowOptions(idColumn: ColumnDescriptor): v_types.RowOptions {
+    return {
+        conditions: [],
+        groupColumns: [],
+        sortColumns: [{
+            column: { parentColumnId: idColumn.id, joinId: 0 },
+            order: v_types.SortOrder.Ascending
+        }],
+    }
+}
--- src/backend/index.ts
+++ src/backend/index.ts
@@ -9,34 +9,9 @@ import {
     ColumnOption
 } from "@intutable/database/dist/column"
 import { insert, select } from "@intutable/database/dist/requests"
-import {
-    ProjectDescriptor,
-    TableDescriptor,
-    TableInfo,
-    ColumnDescriptor,
-} from "@intutable/project-management/dist/types"
-import {
-    createProject,
-    createTableInProject,
-    getTableInfo,
-    createColumnInTable,
-} from "@intutable/project-management/dist/requests"
 import { requests as v_req } from "@intutable/lazy-views/"
-import { types as v_types } from "@intutable/lazy-views"
-import { tableId, viewId } from "@intutable/lazy-views"
 
-import {
-    TableSpec,
-    JoinSpec,
-    Table,
-    PK_COLUMN,
-    PERSONEN,
-    PERSONEN_DATA,
-    ORGANE,
-    ORGANE_DATA,
-    ROLLEN,
-    ROLLEN_DATA,
-} from "./schema"
+import { createExampleSchema, insertExampleData } from "./example/load"
 
 let core: PluginLoader
 const ADMIN_NAME = "admin@dekanat.de"
@@ -62,8 +37,8 @@ export async function init(plugins: PluginLoader){
     // testing data
     if (maybeAdminId === null) {
         console.log("creating and populating example schema")
-        await createExampleSchema(adminId)
-        await insertExampleData()
+        await createExampleSchema(core, adminId)
+        await insertExampleData(core)
     } else console.log("skipped creating example schema")
 }
 
@@ -123,126 +98,4 @@ async function configureColumnAttributes(): Promise<void> {
     await Promise.all(customColumns.map(
         c => core.events.request(v_req.addColumnAttribute(c))
     ))
-}
-
-// example data
-
-let personen: Table
-let organe: Table
-let simpleTables: Table[]
-let rollen: Table
-
-async function createExampleSchema(adminId: number): Promise<void> {
-    const project: ProjectDescriptor = await core.events.request(
-        createProject(adminId, "Fakultät MathInf")
-    ) as ProjectDescriptor
-    personen = await createTable(adminId, project.id, PERSONEN)
-    organe = await createTable(adminId, project.id, ORGANE)
-    simpleTables = [ personen, organe ]
-    rollen = await createTable(adminId, project.id, ROLLEN)
-}
-async function createTable(
-    userId: number,
-    projectId: number,
-    table: TableSpec
-): Promise<Table> {
-    const baseTable: TableDescriptor = await core.events.request(
-        createTableInProject(
-            userId,
-            projectId,
-            table.name,
-            table.columns.map(c => c.baseColumn)
-        )
-    ) as TableDescriptor
-    const tableInfo = await core.events.request(
-        getTableInfo(baseTable.id)
-    ) as TableInfo
-    const idColumn = tableInfo.columns.find(c => c.name === PK_COLUMN)
-    const viewColumns: v_types.ColumnSpecifier[] = table.columns.map(c => {
-        const baseColumn = tableInfo.columns.find(parent =>
-            parent.name === c.baseColumn.name)!
-        return {
-            parentColumnId: baseColumn.id,
-            attributes: c.attributes
-        }
-    })
-    const tableView = await core.events.request(
-        v_req.createView(
-            tableId(baseTable.id),
-            table.name,
-            { columns: viewColumns, joins: [] },
-            EMPTY_ROW_OPTIONS
-        )
-    ) as v_types.ViewDescriptor
-    const filterView = await core.events.request(
-        v_req.createView(
-            viewId(tableView.id),
-            "Standard",
-            { columns: [], joins: [] },
-            baseRowOptions(idColumn)
-        )
-    )
-    const tableDescriptors = { baseTable, tableView, filterView }
-    await Promise.all(
-        table.joins.map(j => addJoin(tableDescriptors, j))
-    )
-    return tableDescriptors
-}
-
-async function addJoin(table: Table, join: JoinSpec): Promise<void> {
-    const fk = await core.events.request(
-        createColumnInTable(
-            table.baseTable.id,
-            join.fkColumn.name,
-            join.fkColumn.type
-        )
-    ) as ColumnDescriptor
-    const foreignTable = simpleTables.find(
-        t => t.tableView.name === join.table)!
-    const info = await core.events.request(
-        v_req.getViewInfo(foreignTable.tableView.id)
-    ) as TableInfo
-    const pk = info.columns.find(c => c.name === join.pkColumn)!
-    const foreignColumns = join.linkColumns.map(l => {
-        const parentColumn = info.columns.find(c => c.name === l.name)!
-        return {
-            parentColumnId: parentColumn.id,
-            attributes: l.attributes
-        }
-    })
-    await core.events.request(
-        v_req.addJoinToView(table.tableView.id, {
-            foreignSource: viewId(foreignTable.tableView.id),
-            on: [fk.id, "=", pk.id],
-            columns: foreignColumns
-        })
-    )
-}
-
-async function insertExampleData(): Promise<void> {
-    await Promise.all(PERSONEN_DATA.map(r => core.events.request(
-        insert(personen.baseTable.key, r)
-    )))
-    await Promise.all(ORGANE_DATA.map(r => core.events.request(
-        insert(organe.baseTable.key, r)
-    )))
-    await Promise.all(ROLLEN_DATA.map(r => core.events.request(
-        insert(rollen.baseTable.key, r)
-    )))
-}
-
-const EMPTY_ROW_OPTIONS: v_types.RowOptions = {
-    conditions: [],
-    groupColumns: [],
-    sortColumns: []
-}
-function baseRowOptions(idColumn: ColumnDescriptor): v_types.RowOptions {
-    return {
-        conditions: [],
-        groupColumns: [],
-        sortColumns: [{
-            column: { parentColumnId: idColumn.id, joinId: 0 },
-            order: v_types.SortOrder.Ascending
-        }],
-    }
 }
