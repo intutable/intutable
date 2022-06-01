56354838e70cca2d09deed5c67ab49c0f0e7ca96
Kilian Folger <kilulafo@gmail.com>
2022-06-01 16:28:22 +0200

    chore: ignore backend dist folders

--- src/backend/startup/core.ts
+++ src/backend/startup/core.ts
@@ -5,7 +5,7 @@ import process from "process"
 
 const PLUGIN_PATHS = [
     path.join(process.cwd(), "node_modules/@intutable/*"),
-    path.join(__dirname, "../../../dekanat-app-plugin"),
+    path.join(__dirname, "../../dekanat-app-plugin"),
 ]
 const PG_PORT = 5432
 const RETRIES = Math.pow(2, 30)
