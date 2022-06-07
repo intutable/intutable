de6d01c1446aeb6182814748e2e75738fe702043
Kilian Folger <kilulafo@gmail.com>
2022-06-07 21:47:19 +0200

    fix: error on switching to a view with fewer filters

--- src/components/DataGrid/Toolbar/ToolbarItems/View/EditFilters.tsx
+++ src/components/DataGrid/Toolbar/ToolbarItems/View/EditFilters.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import FilterListIcon from "@mui/icons-material/FilterList"
 import CloseIcon from "@mui/icons-material/Close"
 import DeleteIcon from "@mui/icons-material/Delete"
@@ -16,9 +16,10 @@ import {
     Box,
 } from "@mui/material"
 
-import { ColumnInfo } from "@intutable/lazy-views/dist/types"
+import { ColumnInfo, ViewDescriptor } from "@intutable/lazy-views/dist/types"
 import { SimpleFilter, FILTER_OPERATORS } from "@backend/condition"
 import { isInternalColumn } from "api/utils/parse/column"
+import { useAPI } from "context/APIContext"
 import { useTable } from "hooks/useTable"
 import { useView } from "hooks/useView"
 import { useSnacki } from "hooks/useSnacki"
@@ -115,16 +116,34 @@ type FilterEditorProps = {
     onUpdateFilters: (newFilters: SimpleFilter[]) => Promise<void>
 }
 
+/**
+ * We want the user to be able to save and apply filters without the
+ * incomplete ones they are also editing to be deleted on each re-render.
+ * To this end, we keep a list of "WIP filters" that stores filters which
+ * cannot be committed to the view yet. If the view's "active" filters change,
+ * the new array is merged with the WIP filters.
+ */
 const FilterEditor: React.FC<FilterEditorProps> = props => {
+    /**
+     * If the view changes, our wip-nulls ~ active filters invariant
+     * (see {@link wipFilters} cannot be maintained, so close the editor.
+     */
+    const { view } = useAPI()
+    const viewRef = useRef<ViewDescriptor | null>()
+    useEffect(() => {
+        viewRef.current = view
+    }, [])
+    const prevView = viewRef.current
+    useEffect(() => {
+        if (prevView && prevView.id !== view?.id) props.onHandleCloseEditor()
+    }, [view])
 
-    /** 
-     * We want the user to be able to save and apply filters without the
-     * incomplete ones they are also editing to be deleted on each re-render.
-     * To this end, we keep a list of "WIP filters", where all incomplete
-     * filters are stored directly in the array, and the real, back-end
-     * filters are simply nulls that will be populated on render.
-     * INVARIANT: the number of nulls in `wipFilters` <= number of
-     * elements in `props.activeFilters`
+    /**
+     * The trick is that `wipFilters` is as long as there are filters in total,
+     * and the active filters' positions are kept free with `null`.
+     * important invariant: the number of nulls in `wipFilters` <= number of
+     * elements in `props.activeFilters`. This is also why we close the
+     * editor when the current view changes.
      */
     const [wipFilters, setWipFilters] = useState<(WipFilter | null)[]>(
         props.activeFilters.length > 0
@@ -135,13 +154,14 @@ const FilterEditor: React.FC<FilterEditorProps> = props => {
 
     /**
      * Merge {@link wipFilters} with the active filters from the back-end
-     * to get the currently-displayed filter list.
+     * to get set of filters to display.
      */
     useEffect(() => {
         if (!props.activeFilters || !wipFilters) return
+
         const displayFilters = new Array(wipFilters.length)
-        for(let aI = 0, wI = 0; wI < wipFilters.length; wI++){
-            if (wipFilters[wI] === null){
+        for (let aI = 0, wI = 0; wI < wipFilters.length; wI++) {
+            if (wipFilters[wI] === null) {
                 displayFilters[wI] = props.activeFilters[aI]
                 aI++
             } else {
@@ -151,9 +171,8 @@ const FilterEditor: React.FC<FilterEditorProps> = props => {
         setFilters(displayFilters)
     }, [props.activeFilters, wipFilters])
 
-    const handleAddFilter = () => setWipFilters(
-        prev => prev.concat(newEmptyWipFilter())
-    )
+    const handleAddFilter = () =>
+        setWipFilters(prev => prev.concat(newEmptyWipFilter()))
 
     /**
        Righto, uh. if it's a WIP filter, easy going. If not... we need
@@ -162,16 +181,17 @@ const FilterEditor: React.FC<FilterEditorProps> = props => {
     const handleDeleteFilter = async (index: number): Promise<void> => {
         if (!filters) return
         // if it's a WIP filter, just delete that
-        setWipFilters(prev => arrayRemove(prev, index))
-
-        // find index within the active filters:
-        const aIndex = wipFilters
-            .slice(0, index + 1)
-            .filter(f => f === null)
-            .length
-            - 1
-        const newFilters = arrayRemove(props.activeFilters, aIndex)
-        return props.onUpdateFilters(newFilters)
+        if (wipFilters[index] !== null)
+            setWipFilters(prev => arrayRemove(prev, index))
+        else {
+            // find index within the active filters:
+            const aIndex =
+                wipFilters.slice(0, index + 1).filter(f => f === null).length -
+                1
+            setWipFilters(prev => arrayRemove(prev, index))
+            const newFilters = arrayRemove(props.activeFilters, aIndex)
+            return props.onUpdateFilters(newFilters)
+        }
     }
 
     const handleCommitFilter = async (
