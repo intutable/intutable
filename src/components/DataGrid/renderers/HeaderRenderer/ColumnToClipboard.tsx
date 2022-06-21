f069ff5c3e5d0a307d8cf9944f34c0a23a11c49c
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-06-21 09:23:51 +0200

    fix: copy selected rows to clipboard

--- src/components/DataGrid/renderers/HeaderRenderer/ColumnToClipboard.tsx
+++ src/components/DataGrid/renderers/HeaderRenderer/ColumnToClipboard.tsx
@@ -2,13 +2,11 @@ import { ColumnInfo } from "@intutable/lazy-views/dist/types"
 import ContentPasteIcon from "@mui/icons-material/ContentPaste"
 import { ListItemIcon, ListItemText, MenuItem } from "@mui/material"
 import { useSelectedRows } from "context/SelectedRowsContext"
-import { checkPrimeSync } from "crypto"
 import { useSnacki } from "hooks/useSnacki"
-import { useTable } from "hooks/useTable"
 import { useView } from "hooks/useView"
-import React from "react"
+import React, { useCallback } from "react"
 import { HeaderRendererProps } from "react-data-grid"
-import { Row } from "types"
+import { Column, Row } from "types"
 import { columnToClipboard } from "utils/columnToClipboard"
 import { isValidMailAddress } from "utils/isValidMailAddress"
 
@@ -25,7 +23,7 @@ export const ColumnToClipboard: React.FC<ColumnToClipboardProps> = props => {
     const { data: viewData } = useView()
     const { snackInfo } = useSnacki()
 
-    const handleCopyToClipboard = () => {
+    const handleCopyToClipboard = useCallback(() => {
         const viewColInfo = viewData?.metaColumns.find(
             c => c.parentColumnId === col.id
         )
@@ -41,26 +39,22 @@ export const ColumnToClipboard: React.FC<ColumnToClipboardProps> = props => {
             headerRendererProps.allRowsSelected === false &&
             selectedRows.size > 0
         ) {
-            // BUG: does not work
             values = viewData!.rows
                 .map(row => {
-                    if (
-                        row[col!.key] != null &&
-                        selectedRows.has(row.__rowIndex__)
-                    )
-                        return row
+                    const value = row[viewColInfo!.key]
+                    if (selectedRows.has(row.__rowIndex__)) return value
                 })
                 .filter(e => e != null)
         }
 
         // filter invalid emails
-        if (col?.attributes.editor === "email")
-            values = values?.filter(isValidMailAddress)
+        if ((col.attributes as Column.SQL)._cellContentType === "email")
+            values = values.filter(isValidMailAddress)
 
         columnToClipboard(values as (string | boolean | number)[])
 
         snackInfo("In die Zwischenablage kopiert!")
-    }
+    }, [col, headerRendererProps, selectedRows, viewData, snackInfo])
 
     return (
         <MenuItem onClick={handleCopyToClipboard}>
