617be97a918648eca695d4ecc540239112048506
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-06-23 15:33:54 +0200

    fix: first approach

--- src/context/SelectedRowsContext.tsx
+++ src/context/SelectedRowsContext.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import { useAPI } from "./APIContext"
 
 export type SelectedRowsContextProps = {
     selectedRows: ReadonlySet<number>
@@ -21,6 +22,25 @@ export const SelectedRowsContextProvider: React.FC<{
     const [selectedRows, setSelectedRows] = useState<ReadonlySet<number>>(
         () => new Set()
     )
+
+    // move in other branch
+
+    const resetSelectedRows = () => setSelectedRows(() => new Set())
+
+    // TODO: test if this works
+    const { table } = useAPI()
+    useEffect(() => resetSelectedRows(), [table])
+
+    // TODO: issue #100 can be fixed here
+
+    // solution 1
+    // observe route changes: when a table is changed, reset the rows
+    // but only if the table switches to another, because we want to keep
+    // the state otherwise
+
+    // solution 2
+    // listen to the APIContext an whenever a table gets changed,
+    // reset the state
 
     return (
         <SelectedRowsContext.Provider
