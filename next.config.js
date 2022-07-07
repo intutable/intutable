da981a1eb8bdaa3275d53f87110a0549bbe96916
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-07-07 13:11:52 +0200

    chose: adjust next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
     enabled: process.env.ANALYZE === "true",
 })
+
+// @ts-check
 /** @type {import('next').NextConfig} */
 module.exports = withBundleAnalyzer({
     reactStrictMode: true,
