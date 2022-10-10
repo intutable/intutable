bfa26be8458887e16ef9c53f1d34d60eaa7d4212
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-10-10 10:53:12 +0200

    delete old files

--- gui/src/api/utils/parse/utils.ts
+++
@@ -1,7 +0,0 @@
-import { types as lv } from "@intutable/lazy-views"
-
-const indexKey = "__columnIndex__"
-export const byIndex = (a: lv.ColumnInfo, b: lv.ColumnInfo) =>
-    (a.attributes[indexKey] as number) > (b.attributes[indexKey] as number)
-        ? 1
-        : -1
