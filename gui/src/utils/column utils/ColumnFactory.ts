1c05a8c8e5f0a4b2b835d74f78d3a23aed584075
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-11-10 11:18:31 +0100

    feat(ColumnFactory): add default values

--- gui/src/utils/column utils/ColumnFactory.ts
+++ gui/src/utils/column utils/ColumnFactory.ts
@@ -1,8 +1,74 @@
 import { Column } from "types"
+import { Text } from "@datagrid/Cells/components/Text"
 
 export type ColumnFactoryProps = Pick<Column.Serialized, "name" | "cellType">
 
+// properties that can be set programmatically from the frontend
+type SettableColumnProps = keyof Omit<
+    Column.Serialized,
+    "key" | "id" | "isUserPrimaryKey" | "index"
+>
+
+// properties that the user can set directly & arbitrarily
+type UserSettableColumnProps = Exclude<
+    SettableColumnProps,
+    "kind" | "minWidth" | "maxWidth"
+>
+
 export class ColumnFactory {
+    static readonly DEFAULT_COLUMN: Pick<
+        Column.Serialized,
+        SettableColumnProps
+    > = {
+        name: "",
+        kind: "standard",
+        hidden: false,
+        cellType: new Text().brand,
+        width: undefined,
+        minWidth: undefined,
+        maxWidth: undefined,
+        cellClass: undefined,
+        headerCellClass: undefined,
+        summaryCellClass: undefined,
+        summaryFormatter: undefined,
+        groupFormatter: undefined,
+        editable: true,
+        colSpan: undefined,
+        frozen: false,
+        resizable: true,
+        sortable: true,
+        sortDescendingFirst: false,
+    }
+    static readonly USER_DEFAULT_COLUMN: Pick<
+        Column.Serialized,
+        UserSettableColumnProps
+    > = {
+        name: "",
+        hidden: false,
+        cellType: new Text().brand,
+        width: undefined,
+        cellClass: undefined,
+        headerCellClass: undefined,
+        summaryCellClass: undefined,
+        summaryFormatter: undefined,
+        groupFormatter: undefined,
+        editable: true,
+        colSpan: undefined,
+        frozen: false,
+        resizable: true,
+        sortable: true,
+        sortDescendingFirst: false,
+    }
+
     constructor(public properties: ColumnFactoryProps) {}
-    // TODO: implement
+
+    /**
+     * Here is some space for further modifications. Scenarios could be:
+     * • Manage what properties can be changed, especially if they are restricted in any way
+     * • …
+     *
+     * The class intendens to be instantiated at the highest possible level (e.g. some component),
+     * at then passed down to the lowest possible level (e.g. a hook).
+     * The necessary properties are then passed down through network requests (by `properties` property).
+     */
 }
