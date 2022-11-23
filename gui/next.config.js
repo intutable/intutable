426ad10674b901a51358b664aeae0a500342daec
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-11-23 09:25:02 +0100

    chore: add prettier as dev dep

--- gui/next.config.js
+++ gui/next.config.js
@@ -17,7 +17,7 @@ module.exports = withBundleAnalyzer({
         dirs: ["src"],
         ignoreDuringBuilds: true,
     },
-    compiler: { removeConsole: false }
+    compiler: { removeConsole: false },
     // experimental: {
     // should improve the mui imports but does not work yet
     // modularizeImports: {
