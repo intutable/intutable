3baaab65f46a8756a88b73ff64397ccc85a1029b
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2022-12-15 20:54:11 +0100

    fix: mountInputMasks

--- shared/src/input-masks/index.ts
+++ shared/src/input-masks/index.ts
@@ -1,9 +1,7 @@
-import { ViewData, ViewInfo } from "@intutable/lazy-views"
+import { ViewInfo } from "@intutable/lazy-views"
 import { SerializedViewData } from "src/types"
 import * as DB from "./database"
 import { InputMask } from "./types"
-
-type UNSAFE_ViewData = ViewData & { inputMasks: InputMask[] }
 
 export const getInputMasksFor = (view: ViewInfo): InputMask[] =>
     DB.getAll().filter(mask => mask.origin.view === view.descriptor.id || mask.origin.view === "*")
@@ -11,5 +9,5 @@ export const getInputMasksFor = (view: ViewInfo): InputMask[] =>
 export const mountInputMasks = (view: SerializedViewData, inputMasks: InputMask[]): SerializedViewData =>
     ({
         ...view,
-        inputMasks: [...(view as unknown as UNSAFE_ViewData).inputMasks, ...inputMasks],
+        inputMasks,
     } as SerializedViewData)
