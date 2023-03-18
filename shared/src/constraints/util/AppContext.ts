53d7d3ac9ac34f19475f8281890b64448385e984
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2023-03-18 16:26:08 +0100

    fix: import path

--- shared/src/constraints/util/AppContext.ts
+++ shared/src/constraints/util/AppContext.ts
@@ -1,8 +1,9 @@
 import type { ViewDescriptor } from "@intutable/lazy-views"
 import type { ProjectDescriptor, TableDescriptor } from "@intutable/project-management/dist/types"
-import { Row, SerializedViewData } from "src/types"
+import { Row, SerializedViewData } from "../../types"
 import type { InputMask } from "../../input-masks/types"
 
+// eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace AppContext {
     export type State = {
         project: ProjectDescriptor
