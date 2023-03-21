5c5ca64135f21853270f6200d82eac5d117f85f4
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2023-03-21 16:35:56 +0100

    doc: add comment

--- shared/src/constraints/util/AppContext.ts
+++ shared/src/constraints/util/AppContext.ts
@@ -2,9 +2,22 @@ import type { ViewData, ViewDescriptor } from "@intutable/lazy-views"
 import type { ProjectDescriptor, TableDescriptor } from "@intutable/project-management/dist/types"
 import { Row, SerializedViewData } from "../../types"
 import type { InputMask } from "../../input-masks/types"
-import { SerializedLogEntry } from "../dos/Log/Log"
+import type { SerializedLogEntry } from "../dos/Log/Log"
 
-// TODO: if the constraints are imported and executed inside the frontend, we can directly use hooks and their states instead of forwarding them here
+/**
+ * If the constraints were imported and executed only inside the frontend,
+ * we could directly use hooks and their states instead of forwarding them –
+ * meaning we could access the frontends scope instead of borrowing injected
+ * methods and variables.
+ *
+ * Since the constraints are not only conditions and executements,
+ * but also generate a serialized definition, we probably rely on
+ * these injected states and dispatchers.
+ *
+ * Most of them are just the signatures of hook properties.
+ * Some are methods living inside the context that validates constraints.
+ */
+// eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace AppContext {
     export type State = {
         project: ProjectDescriptor
