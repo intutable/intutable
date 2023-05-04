b0221b2fecc0d3fca0ab0eea4475f3ab793ef4d8
Luis Schulte <8312466-lsg551@users.noreply.gitlab.com>
2023-05-04 10:28:41 +0200

    chore: use loading overlay with pending rows

--- gui/src/components/DataGrid/Toolbar/ToolbarItems/AddRow/AddPendingRow.tsx
+++ gui/src/components/DataGrid/Toolbar/ToolbarItems/AddRow/AddPendingRow.tsx
@@ -1,13 +1,14 @@
 import AddIcon from "@mui/icons-material/TableRows"
 import { LoadingButton } from "@mui/lab"
+import { LoadingOverlay } from "components/LoadingOverlay"
 import { usePendingRow } from "hooks/usePendingRow"
 import { useSnacki } from "hooks/useSnacki"
-import React from "react"
+import React, { useEffect } from "react"
 
 export const AddPendingRow: React.FC<{ text?: string }> = ({ text }) => {
     const { snackError, snackInfo, snackSuccess } = useSnacki()
 
-    const { createPendingRow, pending } = usePendingRow()
+    const { createPendingRow, pending, loading } = usePendingRow()
 
     const handleCreateRow = async () => {
         try {
@@ -19,16 +20,21 @@ export const AddPendingRow: React.FC<{ text?: string }> = ({ text }) => {
         }
     }
 
+    useEffect(() => {
+        console.log("pending:", pending)
+    }, [pending])
+
     return (
         <>
             <LoadingButton
-                loading={pending}
+                loading={loading}
                 loadingIndicator={"Wird erstellt ..."}
                 startIcon={<AddIcon />}
                 onClick={handleCreateRow}
             >
                 {text || "Neuer Eintrag"}
             </LoadingButton>
+            <LoadingOverlay open={loading} />
         </>
     )
 }
