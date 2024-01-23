8f69feb63f959ae682e9ec8dc19e5bb639e8bad3
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2024-01-23 21:43:50 +0100

    feat(system-plugin): register endpoints for tables, views and auth

--- libs/plugins/system-plugin/src/index.ts
+++ libs/plugins/system-plugin/src/index.ts
@@ -1,5 +1,8 @@
 import {PluginLoader} from "../../../core";
 import {initProjectEndpoints} from "./modules/projects";
+import {initTableEndpoints} from "./modules/tables";
+import {initViewEndpoints} from "./modules/views";
+import {initAuthEndpoints} from "./modules/auth";
 
 let core: PluginLoader;
 
@@ -7,4 +10,7 @@ export async function init(pluginLoader: PluginLoader) {
     core = pluginLoader
 
     await initProjectEndpoints(core)
+    await initTableEndpoints(core)
+    await initViewEndpoints(core)
+    await initAuthEndpoints(core)
 }
